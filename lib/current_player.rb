2e50b33a1b069f5c629b8332c6728a680019ca58
Netaly Ramirez <21netaly@gmail.com>
2020-03-09 23:39:42 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 # check each indx of array
-# if that index is X/O counter +=1 else 'turn' 
+# if that index is X/O counter +=1 else 'turn'
   def turn_count(board)
     count = 0
   board.each do |curr_play|
