a419fc2191134b517e027aa7e814700af429f9f8
Netaly Ramirez <21netaly@gmail.com>
2020-03-10 00:41:49 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -16,10 +16,6 @@ end
 
 
 def current_player(board)
-  turn = turn_count(board)
-  if turn % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+turn = turn_count(board)
+turn%2 ==0?  'X': 'O'
 end
