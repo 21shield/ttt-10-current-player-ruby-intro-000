dad8f37a68142e19521be64686680016a8ef2763
Netaly Ramirez <21netaly@gmail.com>
2020-03-10 00:35:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,14 +4,12 @@
 
 def turn_count(board)
   count = 0
-  while count <= board.length-1
     board.each do |char|
       if char == 'X' || char == "O"
         count +=1
       else
         count +=0
       end
-    end
   end
   return count
 end
